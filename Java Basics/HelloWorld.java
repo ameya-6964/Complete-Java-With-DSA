c706cc042939e6a8c782ddc103d24d1464e84a10
ameya-6964 <belvalkar7@gmail.com>
2023-01-29 14:45:04 +0530

    Added Simple Void Method

--- Java Basics/HelloWorld.java
+++ Java Basics/HelloWorld.java
@@ -2,5 +2,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        greet(); // Method Calling Statement
+    }
+
+    public static void greet() {
+        System.out.println("Hello User :)");
     }
 }

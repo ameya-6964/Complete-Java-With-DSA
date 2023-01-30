d0f67060adc52654acdc38cc5c0791eb462da863
ameya-6964 <belvalkar7@gmail.com>
2023-01-30 14:47:18 +0530

    created Method With return Type

--- Java Basics/HelloWorld.java
+++ Java Basics/HelloWorld.java
@@ -3,9 +3,16 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         greet(); // Method Calling Statement
+        int result = addTwoNumbers(22, 33);
+        System.out.println(result);
     }
 
     public static void greet() {
         System.out.println("Hello User :)");
+    }
+
+    public static int addTwoNumbers(int a, int b) {
+        int c = a + b;
+        return c;
     }
 }

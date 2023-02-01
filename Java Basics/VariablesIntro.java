f822e7595bfc6eff32a2249a126bb5d4c288c8e5
ameya-6964 <belvalkar7@gmail.com>
2023-02-01 15:04:49 +0530

    added Variables

---
+++ Java Basics/VariablesIntro.java
@@ -0,0 +1,11 @@
+public class VariablesIntro {
+    public static void main(String[] args) {
+        int a = 10;
+
+        int b = 15;
+
+        int c = a + b;
+
+        System.out.println(c); // 25
+    }
+}

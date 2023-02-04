df5d826f1e507cc2b5bbdb9509867061bbacbb30
ameya-6964 <belvalkar7@gmail.com>
2023-02-05 01:31:26 +0530

    Added Pattern 1

---
+++ Java Patterns/Pattern1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+        sc.close();
+    }
+}
+
+/*
+ * Enter Number
+ * 6
+ ******
+ ******
+ ******
+ ******
+ ******
+ */
